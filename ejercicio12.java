96bf2eea02caa58b5b293383b7d81ef5b2794b1f
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-11 22:52:44 -0700

    Add files via upload

---
+++ ejercicio12.java
@@ -0,0 +1,10 @@
+ import java.util.*;
+ public class ejercicio12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cadena;
+        System.out.println("Introduce un nombre: ");
+        cadena = sc.nextLine();
+        System.out.println("Buenos Días " + cadena);
+    }
+}

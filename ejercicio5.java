c49e5cdd5057c692441ecac57d4415bf52075de2
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-11 22:32:55 -0700

    Add files via upload

--- ejercicio5.java
+++ ejercicio5.java
@@ -1,11 +1,17 @@
-//Ricardo Jimenez 07/02/19 12:49
-public class ejercicio5
-{
-    public static void main(String args[])
-    {
-        for(int i = 0; i <= 12; i++)//contador que incrementa desde el 0 hasta el 12
-        {
-            System.out.print("12 * "+ i + " = " + 12 * i + "\n");//imprime la tabla de multiplicar del 12
-        }
-    }
+import java.util.Scanner;
+ 
+public class ejercicio5 {
+ 
+ public static void main(String[] args) {
+  Scanner in = new Scanner(System.in);
+   
+  System.out.print("Ingresa el primer numero: ");
+  int num1 = in.nextInt();
+   
+  System.out.print("Ingresa el segundo numero: ");
+  int num2 = in.nextInt();
+   
+  System.out.println(num1 + " x " + num2 + " = " + num1 * num2);
+ }
+ 
 }

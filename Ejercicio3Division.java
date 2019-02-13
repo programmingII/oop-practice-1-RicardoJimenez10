b1b481fab062ed2f6c279081a85e52505c9cb455
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-12 23:51:39 -0700

    Add files via upload

---
+++ Ejercicio3Division.java
@@ -0,0 +1,9 @@
+
+//Ricardo Jimenez 07/02/19 12:42
+public class Ejercicio3Division {
+ 
+ public static void main(String[] args) {
+  System.out.println(500/2); //imprime el resultado de la division
+ }
+ 
+}

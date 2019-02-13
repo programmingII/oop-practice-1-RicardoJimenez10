fa22ee98f7503fbdf42d077a446634297645e6db
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-12 23:38:30 -0700

    Add files via upload

---
+++ Ejercicio1Hola.java
@@ -0,0 +1,7 @@
+//Ricardo Jimenez 12:25pm 05/02/19
+public class Ejercicio1Hola {
+ public static void main(String[] args) { //esta funcion para que no regrese ningun valor y para ejecutar en la consola
+  System.out.println("Hola/n Richy"); //Imprime en pantalla en saludo
+ 	}
+ 
+	}

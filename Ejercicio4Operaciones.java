02e85526bcd2aa6a766aba5d95d5aa6fd7aac4a7
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-12 23:53:59 -0700

    Add files via upload

---
+++ Ejercicio4Operaciones.java
@@ -0,0 +1,11 @@
+//Ricardo Jimenez 07/02/19 12:46
+public class Ejercicio4Operaciones {
+ 
+ public static void main(String[] args) {
+  System.out.println(-5 + 8 * 6);//imprime resultado de la operacion
+  System.out.println((55+9) % 9);//imprime resultado de la operacion
+  System.out.println(20 + -3*5 / 8);//imprime resultado de la operacion
+  System.out.println(5 + 15 / 3 * 2 - 8 % 3);//imprime resultado de la operacion
+ }
+ 
+}

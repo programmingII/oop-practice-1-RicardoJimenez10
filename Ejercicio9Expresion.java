889c01d8597877868424e9738ae5bc21af37b67a
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-13 15:06:17 -0700

    Add files via upload

---
+++ Ejercicio9Expresion.java
@@ -0,0 +1,8 @@
+//Ricardo jimenez 07/02/19 1:29pm
+public class Ejercicio9Expresion {
+ 
+    public static void main(String[] arg) {
+
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));//realiza una operacion matematica
+    }
+}

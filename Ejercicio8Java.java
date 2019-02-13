4e06cfbab88fc22d7edf6e40040f8263be375fa9
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-13 15:00:52 -0700

    Add files via upload

---
+++ Ejercicio8Java.java
@@ -0,0 +1,11 @@
+//Ricardo Jimenez 07/02/19 1:20pm
+public class Ejercicio8Java {
+ 
+    public static void main(String[] args) {
+
+        System.out.println("   J    a   v     v  a ");//imprime la palabra java
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+    }
+}

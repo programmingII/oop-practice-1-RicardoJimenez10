b80e8a5ed039f5f54e314b7dd50ab9504acf538b
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-12 23:51:06 -0700

    Delete ejercicio3.java

--- ejercicio3.java
+++
@@ -1,8 +0,0 @@
-//Ricardo Jimenez 07/02/19 12:42
-public class ejercicio3 {
- 
- public static void main(String[] args) {
-  System.out.println(500/2); //imprime el resultado de la division
- }
- 
-}

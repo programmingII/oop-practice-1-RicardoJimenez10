2617047069b939875b455fa5264c782da942a382
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-13 15:11:04 -0700

    Add files via upload

---
+++ Ejercicio10Formula.java
@@ -0,0 +1,9 @@
+//Ricardo jimenez 13/02/19 3:07pm
+public class Ejercicio10Formula {
+ 
+    public static void main(String[] args) {
+
+        double pi = 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));//realiza la formila de pi
+        System.out.println(pi); //imprime el valor de pi
+    }
+}

cac6be9cdd26752d9ba2a1aea37bf50008b3cc24
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-14 12:50:50 -0700

    Update Ejercicio17Binarios.java

--- Ejercicio17Binarios.java
+++ Ejercicio17Binarios.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 public class Ejercicio17Binarios {
  public static void main(String[] args)
  {
-  long binary1, binary2;
-  int i = 0, remainder = 0;
-  int[] sum = new int[20];
+  long binary1, binary2;//el long sirve para poder teclear numeros largos de varios digitos 
+  int i = 0, remainder = 0;//el valor de i empieza de cero 
+  int[] sum = new int[20];//sirve para teclear numeros igual o menores a 20
   Scanner in = new Scanner(System.in);
 
-  System.out.print("Ingresa el primer numero binario: ");
-  binary1 = in.nextLong();
-  System.out.print("Ingresa el segundo numero binario: ");
-  binary2 = in.nextLong();
+  System.out.print("Ingresa el primer numero binario: ");//ingresamos el primer numero
+  binary1 = in.nextLong();//guarda el numero tecleado
+  System.out.print("Ingresa el segundo numero binario: ");//ingresamos el segundo numero
+  binary2 = in.nextLong();//guarda el segundo numero binario
 
-  while (binary1 != 0 || binary2 != 0) 
+  while (binary1 != 0 || binary2 != 0) //si la condicion es verdadera que el numero banario es diferente a cero y el numero binario 2 es diferente a cero entonces se realiza la funcion
   {
-   sum[i++] = (int)((binary1 % 10 + binary2 % 10 + remainder) % 2);
-   remainder = (int)((binary1 % 10 + binary2 % 10 + remainder) / 2);
+   sum[i++] = (int)((binary1 % 10 + binary2 % 10 + remainder) % 2);//en esta linea incrementa el i  
+   remainder = (int)((binary1 % 10 + binary2 % 10 + remainder) / 2);//
    binary1 = binary1 / 10;
    binary2 = binary2 / 10;
   }

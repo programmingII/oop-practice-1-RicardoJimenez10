47ca3f07700eee97477032b0946b79ba574053be
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-13 23:31:45 -0700

    Add files via upload

---
+++ Ejercicio15Intercambio.java
@@ -0,0 +1,14 @@
+public class Ejercicio15Intercambio {
+ 
+ public static void main(String[] args) {
+     
+   int a, b, temp;
+   a = 15;
+   b = 27;
+   System.out.println("Antes del intercambio : a, b = "+a+", "+ + b);
+   temp = a;
+   a = b;
+   b = temp;   
+  System.out.println("Despues del intercambio : a, b = "+a+", "+ + b);
+ }
+ }

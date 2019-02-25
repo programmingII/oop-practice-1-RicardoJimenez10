e6345dc406b268f930f52920a81630fa63b34e80
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-25 14:36:24 -0700

    Add files via upload

---
+++ Ejercicio48Numeros1al100.java
@@ -0,0 +1,10 @@
+import java.util.*;
+ public class Ejercicio48Numeros1al100 {
+     public static void main(String[] args){
+	for (int i = 1; i < 101; i++) {
+			if (i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+    }
+}

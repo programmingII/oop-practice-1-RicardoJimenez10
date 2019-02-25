f4759de500f77dfe00fa4d6d369f6bb82dc25f08
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-25 14:32:15 -0700

    Add files via upload

---
+++ Ejercicio46Hora.java
@@ -0,0 +1,6 @@
+public class Ejercicio46Hora {
+  
+  public static void main(String[] args){
+         System.out.format("\nDia y hora del dia de hoy es : %tc%n\n", System.currentTimeMillis());
+    }
+}

c3d1c3c45a255b777a3c3046a27af4bc8d8a81a4
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-27 14:52:44 -0700

    Update Ejercicio57.java

--- Ejercicio57.java
+++ Ejercicio57.java
@@ -2,20 +2,21 @@ import java.util.*;
 
 public class Ejercicio57{
 public static void main(String[] args){
-Scanner in=new Scanner(System.in);
-System.out.print("Ingresa un numero entero:");
-int x=in.nextInt();
-System.out.println(result(x));
+Scanner in=new Scanner(System.in);//Se crea el objeto que almacenara datos
+System.out.print("Ingresa un numero entero:");//Nos permite ingresar un numero entero
+int x=in.nextInt();// Se almacena el numero en entero x
+System.out.println(result(x));//Nos imprime el resultado de los enteros
 }
 public static int result(int num){
 int ctr=0;
-for (int i=1;i<=(int)Math.sqrt(num);i++){
-if (num%i==0&& i*i!=num){
-ctr+=2;
-} else if (i*i==num){
-ctr++;
+for (int i=1;i<=(int)Math.sqrt(num);i++){//Ciclo for que vas desde 1 hasta el resultado de la raiz cuadrada de x con incremento de 1
+if (num%i==0&& i*i!=num){//Entra al ciclo solo si el residuo de x entre el valor de i es cero y  i al cuadrado es diferente de un num
+ctr+=2;//En la variable cont se suma cont mas dos
+} else if (i*i==num){// entra al ciclo solo si i al cuadrado es igual a x
+
+ctr++;//se incrementa uno al contador
 }
 }
-return ctr;
+return ctr;//se regresa el contador si es que falla
 }
 }

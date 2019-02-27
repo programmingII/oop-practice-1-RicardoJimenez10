8bad78414a56557530c856dc967a31a97b851be1
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-27 14:45:40 -0700

    Update Ejercicio59.java

--- Ejercicio59.java
+++ Ejercicio59.java
@@ -1,10 +1,10 @@
-import java.util.*;
+import java.util.*;//nos permite importar alguna clase del paquete que necesitamos para la linea de codigo
  public class Ejercicio59 {
      public static void main(String[] args){	
-     Scanner in = new Scanner(System.in);
-     System.out.print("Input a String: ");
-	 String line = in.nextLine();
-	 line = line.toLowerCase();
-	 System.out.println(line); 
+     Scanner in = new Scanner(System.in);//Nos crea el objeto in para almacenar datos
+     System.out.print("Input a String: ");//Nos permite introducir la operacion 
+	 String line = in.nextLine();//Nos guarda la informacion que hayamos escrito anteriormente
+	 line = line.toLowerCase();//De la linea con el metodo toLowerCase se convierte a minuscula lo que esta antes del punto
+	 System.out.println(line); //Nos imprime la linea que pongamos pero en letras minusculas
 	 }			
 }

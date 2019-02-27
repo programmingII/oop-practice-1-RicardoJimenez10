a4b509e0e059db80e508acdd92f9ad3fa609ba2a
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-02-27 14:33:24 -0700

    Update Ejercicio63.java

--- Ejercicio63.java
+++ Ejercicio63.java
@@ -1,18 +1,19 @@
-import java.util.*;
+import java.util.*;//Nos permite importar alguna clase del paquete que necesitamos para la linea de codigo
+
  public class Ejercicio63 {
  public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input the first number : ");
-        int a = in.nextInt();  
-		System.out.print("Input the second number: ");
-		int b = in.nextInt(); 
-		System.out.println("Result: "+result(a, b));
+        Scanner in = new Scanner(System.in);//Nos crea el objeto in para almacenar datos
+        System.out.print("Input the first number : ");//Nos permite ingresar nuestro primer numero
+        int a = in.nextInt(); //utilizamos el objeto creado de tipo entero para usar el metodo de ingreso de datos, y guardo ese dato en la variable de nuestro primer numero 
+		System.out.print("Input the second number: ");//Nos permite ingresar nuestro segundo numero
+		int b = in.nextInt(); //utilizamos el objeto creado de tipo entero para usar el metodo de ingreso de datos, y guardo ese dato en la variable de nuestro segundo numero
+		System.out.println("Result: "+result(a, b));//Nos imprime el resultado del numero entre a y b
     }
-	public static int result(int x, int y)
+	public static int result(int x, int y)//se almacenan los valores de x & y como enteros
      {  
-	if(x == y)
-		return 0;
+	if(x == y)//si x es igual a y entonces cumple con la funcion if
+		return 0;//nos retorna el valor
 	    if(x % 6 == y % 6)
 		    return (x < y) ? x : y;
 	    return (x > y) ? x : y;

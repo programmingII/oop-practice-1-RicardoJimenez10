4692cea71e7f0604c21880d374f04187db9af6c1
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-03-04 14:29:40 -0700

    Update Ejercicio64.java

--- Ejercicio64.java
+++ Ejercicio64.java
@@ -1,23 +1,26 @@
-import java.util.*;
+import java.util.*;//Nos permite importar alguna clase del paquete que necesitamos para la linea de codigo
+
  public class Ejercicio64 {
  public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input the first number : ");
-        int a = in.nextInt();  
-		System.out.print("Input the second number: ");
-		int b = in.nextInt(); 
-		System.out.println("Result: "+common_digit(a, b));
+        Scanner in = new Scanner(System.in);//Nos crea el objeto in para almacenar datos
+        System.out.print("Input the first number : ");//Ingresamos el primer numero
+        int a = in.nextInt();  //utilizamos el objeto creado de tipo entero para usar el metodo de ingreso de datos, y guardo ese dato en la variable de nuestro primer numero
+		System.out.print("Input the second number: ");//ingresamos en segundo numero
+		int b = in.nextInt(); //utilizamos el objeto creado de tipo entero para usar el metodo de ingreso de datos, y guardo ese dato en la variable de nuestro segundo numero
+		System.out.println("Result: "+common_digit(a, b));//Nos imprime el resultado del numero entre a y b
+    
+
     }
 	
 	public static boolean common_digit(int p, int q)
      {  
-	   if (p<25 || q>75)
-		   return false;
-	   int x = p % 10;
-	   int y = q % 10;
+	   if (p<25 || q>75)//si p es menor a 25 y q es mayor a 75 se cumple la condicion
+		   return false;//si no se cumple la condicion se retorna como falso y se acaba el programa
+	   int x = p % 10;//x es igual a p entre el modular de 10
+	   int y = q % 10;//y es igual a q y el modular entre 10
 	   p /= 10;
 	   q /= 10;
-	   return (p == q || p == y || x == q || x == y);
+	   return (p == q || p == y || x == q || x == y);//entomces se realiza la operacion
      }
 }

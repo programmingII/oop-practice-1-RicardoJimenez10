7573ffea4e5ba1582a0feca410c500b480bbcf45
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-03-04 14:16:44 -0700

    Update Ejercicio50NumerosDivididos.java

--- Ejercicio50NumerosDivididos.java
+++ Ejercicio50NumerosDivididos.java
@@ -1,20 +1,21 @@
 public class Ejercicio50NumerosDivididos {
     	public static void main(String args[]) {
-		System.out.println("\nDividido entre 3: ");		
-		for (int i=1; i<100; i++) {
-			if (i%3==0) 
-			System.out.print(i +", ");			
+		System.out.println("\nDividido entre 3: ");//Nos pide que ingresemos un numero		
+		for (int i=1; i<100; i++) {//Se utiliza ciclo for que recorre los numero desde 1 hasta 100
+			if (i%3==0) //Si el numero i es divisible entre 3 y el residuo es igual a cero entra al ciclo
+			System.out.print(i +", ");//Se imprime el numero almacenado en i				
 		}			
 				
-		System.out.println("\n\nDividido entre 5: ");
-		for (int i=1; i<100; i++) {
-			if (i%5==0) System.out.print(i +", ");			
+		System.out.println("\n\nDividido entre 5: ");//Nos pide que ingresemos un numero
+		for (int i=1; i<100; i++) {//Se utiliza ciclo for que recorre los numero desde 1 hasta 100
+			if (i%5==0) System.out.print(i +", ");//Si el numero i es divisible entre 5 y el residuo es igual a cero entra al ciclo				
 		}
 				
-		System.out.println("\n\nDividido entre 3 y 5: ");			
-		for (int i=1; i<100; i++) {
-			if (i%3==0 && i%5==0) System.out.print(i +", ");			
+		System.out.println("\n\nDividido entre 3 y 5: ");//Nos permite dividir el numero 3 y 5			
+		for (int i=1; i<100; i++) {//Se utiliza ciclo for que recorre los numero desde 1 hasta 100
+			if (i%3==0 && i%5==0) System.out.print(i +", ");//Si el numero i es divisible entre 3 y 5  el residuo es igual a cero entra al ciclo			
 		}
-		System.out.println("\n");
+		System.out.println("\n");//Se imprime el numero almacenado
+  }
   }
 }

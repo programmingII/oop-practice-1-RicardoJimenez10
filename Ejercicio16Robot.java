39ed4695681802396fbea36ab1aa37ffe81a7673
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-03-04 14:36:03 -0700

    Update Ejercicio16Robot.java

--- Ejercicio16Robot.java
+++ Ejercicio16Robot.java
@@ -2,11 +2,11 @@
 public class Ejercicio16Robot {
     public static void main(String[] args)
     {
-        System.out.println(" +\"\"\"\"\"+ ");
-        System.out.println("[| o o |]");
-        System.out.println(" |  ^  | ");
-        System.out.println(" | '-' | ");
-        System.out.println(" +-----+ ");
+        System.out.println(" +\"\"\"\"\"+ ");//imprime los simbolos que se ingresan
+        System.out.println("[| o o |]");//imprime los simbolos que se ingresan
+        System.out.println(" |  ^  | ");//imprime los simbolos que se ingresan
+        System.out.println(" | '-' | ");//imprime los simbolos que se ingresan
+        System.out.println(" +-----+ ");//imprime los simbolos que se ingresan
     }
 	}
 	

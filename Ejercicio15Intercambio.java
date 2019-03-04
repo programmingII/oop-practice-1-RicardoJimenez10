7cffe66e54b67d68f327b9358e1d683c8b9f0432
160016_RicardoJimenez <46793965+RicardoJimenez10@users.noreply.github.com>
2019-03-04 14:35:21 -0700

    Update Ejercicio15Intercambio.java

--- Ejercicio15Intercambio.java
+++ Ejercicio15Intercambio.java
@@ -2,13 +2,13 @@ public class Ejercicio15Intercambio {
  
  public static void main(String[] args) {
      
-   int a, b, temp;
-   a = 15;
-   b = 27;
-   System.out.println("Antes del intercambio : a, b = "+a+", "+ + b);
-   temp = a;
-   a = b;
+   int a, b, temp;//declaramos con enteros a y b
+   a = 15;//declaramos en a sea igual a 15
+   b = 27;//b es igual a 27
+   System.out.println("Antes del intercambio : a, b = "+a+", "+ + b);//imprime el resultado y muestra el valor antes del intercambio
+   temp = a;//nombre de variable local que es igual a a
+   a = b;//a es igual a b
    b = temp;   
-  System.out.println("Despues del intercambio : a, b = "+a+", "+ + b);
+  System.out.println("Despues del intercambio : a, b = "+a+", "+ + b);//se imprime el resultado
  }
  }
